1ae44334596348b0db89cee9cc5e675c4e8f7172
sighmon <simon.loffler@gmail.com>
2022-03-12 12:57:18 +1030

    #1 Post radiation data to api.safecast.org every 30 seconds

--- secrets.tmpl.h
+++ secrets.tmpl.h
@@ -1,2 +1,10 @@
+// WiFi credentials
 const char* SECRET_SSID = "your ssid";
 const char* SECRET_PASSWORD = "your password";
+
+// SafeCast credentials
+const char* SAFECAST_API_URL = "https://api.safecast.org/measurements.json";
+const char* SAFECAST_API_KEY = "";
+const char* SAFECAST_DEVICE_ID = "249"; // https://api.safecast.org/en-US/devices?utf8=✓&model=SEN0463
+const char* SAFECAST_DEVICE_LATITUDE = "your device latitude";
+const char* SAFECAST_DEVICE_LONGITUDE = "your device longitude";
